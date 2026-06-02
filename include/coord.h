7339b37ef7fc86aead2d60fc4a6356655a4c2269
guillermosanchez05 <guillermo1.spgm@gmail.com>
2026-06-02 18:15:49 +0200

    Removed inefficient CoordType struct

--- include/coord.h
+++ include/coord.h
@@ -1,13 +1,3 @@
-
-/**
- * @brief Types that Coord can have: EMPTY, SNAKE or FOOD
- */
-typedef enum {
-    EMPTY,
-    SNAKE,
-    FOOD
-} CoordType;
-
 /**
  * @brief Struct to represent a coordinate in the board,
  * it has an 'x' value, 'y' value and CoordType 'type'.
@@ -15,12 +5,11 @@ typedef enum {
 struct Coord{
     int x;
     int y;
-    CoordType type;
 };
 
 /**
- * @brief Gets the char associated to a Coord coordinate
- * '█' for Snake, '●' for Food and ' ' for empty.
+ * @brief Gets the char associated to a Coord coordinate so it can be
+ * represented: '█' for Snake, '●' for Food and ' ' for empty.
  * 
  * @param coordinate The coordinate whose type will be converted to a character.
  * @return Char associated to the type.
