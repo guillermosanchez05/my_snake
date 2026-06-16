9652e05d98b7cf5efc53fc9ff49f13e21aad4edf
guillermosanchez05 <guillermo1.spgm@gmail.com>
2026-06-16 17:58:31 +0200

    refactor: remove outdated TODO comments from main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,21 +3,6 @@
 
 int main(){
 
-    // TODO:
-    /*
-    
-    Crear Snake (que se inicializa sola con sus posiciones iniciales internas)
-
-    Crear Food (que se inicializa sola con una posición aleatoria interna)
-
-    Crear Board
-
-    Llamar a board.init(snake, food) → el tablero pregunta a snake y food y pinta
-
-    Llamar a board.display() → muestra
-
-    */
-
     // Create the snake (which will be placed on the Board)
     Snake snake;
     snake.init(Board::WIDTH, Board::HEIGHT);
